d3fc88db9c82820dc6395ef6a9eb8ea0b1f5111d
Alfredo <alfreddovillegas@gmail.com>
2022-01-14 11:34:48 -0400

    Add Class DomainEvent for extends all events

---
+++ src/Modules/Shared/domain/DomainEvent.ts
@@ -0,0 +1,15 @@
+import { Uuid } from './value-object/Uuid';
+
+export abstract class DomainEvent {
+  readonly aggregateId: string;
+  readonly eventId: any;
+  readonly occurredOn: Date;
+  readonly eventName: string;
+
+  constructor(eventName: string, aggregateId: string, eventId?: string, occurredOn?: Date) {
+    this.aggregateId = aggregateId;
+    this.eventId = eventId || Uuid.random().value;
+    this.occurredOn = occurredOn || new Date();
+    this.eventName = eventName;
+  }
+}
